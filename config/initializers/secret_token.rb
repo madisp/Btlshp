86cbf52f5f2a3c255dda8c1e5823c90f04e41786
madisp <madis.pink@gmail.com>
2012-02-13 16:25:12 +0200

    Rails skeleton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btlshp::Application.config.secret_token = 'fe17daf46c32e81d5fe588745214b869391a3e29de382108071dc02c9521d3e78944e853495e883ff6f54a3219a897c895007b6801e7429e75a250f2f4df3c5d'
