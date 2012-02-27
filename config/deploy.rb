8837e66f89a35ff7129965f649d0f1dca04d383e
Madis Pink <madis.pink@gmail.com>
2012-02-27 12:17:04 +0200

    Added deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,60 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "btlshp"
+set :repository,  "git://github.com/madisp/Btlshp.git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "btlshp.madisp.com"
+role :app, "btlshp.madisp.com"
+role :db,  "btlshp.madisp.com", :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :user, 'ubuntu'
+set :use_sudo, false
+set :deploy_to, '/var/www/btlshp'
+set :deploy_via, :remote_cache
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ssh_options[:forward_agent] = false
+
+# Passenger restart
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+require 'pathname'
+
+namespace :security do
+  namespace :symlinks do
+    task :setup, :roles => [:app, :web], :except => { :no_release => true } do
+      run "touch #{shared_path}/secret_token.rb"
+    end
+    task :update, :roles => [:app, :web], :except => { :no_release => true } do
+      orig = "#{release_path}/config/initializers/secret_token.rb"
+      run "rm #{orig}"
+      run "ln -nfs #{shared_path}/secret_token.rb #{orig}"
+    end
+  end
+end
+
+namespace :ci do
+  task :append_version, :roles => [:app, :web], :except => { :no_release => true } do
+    env_version = ENV['BUILD_VERSION']
+    if env_version
+      version = env_version.gsub /[^0-9\.A-z\-\_]/, ''
+      if version and version.length > 0
+        run "echo '#{version}' > #{release_path}/.build_version"
+      end
+    end
+  end
+end
+
+before 'deploy:setup' do
+  security.symlinks.setup
+end
+
+before 'deploy:symlink' do
+  security.symlinks.update
+  ci.versions.append_version
+end
